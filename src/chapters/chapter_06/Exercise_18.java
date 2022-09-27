752027c0ad15fd5e7726ff41740a38667002f561
Ali Oğuzhan Türkmen <111460897+oguzzhanturkmen@users.noreply.github.com>
2022-09-27 03:07:45 +0300

    Added chapter_06 exercises

--- src/chapters/chapter_06/Exercise_18.java
+++ src/chapters/chapter_06/Exercise_18.java
@@ -13,9 +13,9 @@ public class Exercise_18 {
         System.out.println("Your password is " + (isValid(pass) ? "valid" : "not valid"));
     }
     public static boolean isValid(String pass){
-        return (pass.length() >= 8 && isConsistsLettersAndDigits(pass) && containsTwoDigits(pass));
+        return (pass.length() >= 8 && ConsistsLettersAndDigits(pass) && containsTwoDigits(pass));
     }
-    public static boolean isConsistsLettersAndDigits(String pass){
+    public static boolean ConsistsLettersAndDigits(String pass){
         boolean isValid = false;
         for(int i = 0; i < pass.length(); i++){
             if ((pass.charAt(i) >= '0' && pass.charAt(i) <= '9') || (pass.charAt(i) >= 'A' && pass.charAt(i) <= 'Z') || (pass.charAt(i) >= 'a' && pass.charAt(i) <= 'z') ){
