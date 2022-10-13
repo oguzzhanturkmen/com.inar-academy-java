9603981dd4d2ce07a1b812d892de0dbd3a0fc4ac
Ali Oğuzhan Türkmen <111460897+oguzzhanturkmen@users.noreply.github.com>
2022-10-13 03:48:40 +0300

    Added chapter_07 exercises

--- src/chapters/chapter_07/Exercise_22.java
+++ src/chapters/chapter_07/Exercise_22.java
@@ -1,4 +1,66 @@
 package chapters.chapter_07;
 
+import java.util.Arrays;
+
 public class Exercise_22 {
+    public static void main(String[] args) {
+        int[] array = new int[8];
+        while(placeQueen(array));
+        System.out.println(Arrays.toString(array));
+        displayChessBoard(array);
+    }
+    public static boolean checkPosition(int queenPlace, int[] array, int loopCounter) {
+
+        for (int i = 0 ,j = loopCounter; i < loopCounter; i++, j--) {
+            if (queenPlace == array[i]) {
+                return false;
+            }
+            if (queenPlace == array[i] + j || queenPlace == array[i] - j) {
+                return false;
+            }
+        }
+        return true;
+    }
+    public static boolean placeQueen(int[] array) {
+        int queenPlace = (int) (Math.random() * 8);
+        array[0] = queenPlace;
+         boolean isTrue = true;
+        int counter = 0;
+        boolean restartLoop = false;
+
+        for (int i = 1; i < array.length; i++) {
+            isTrue = true;
+            while (isTrue) {
+                queenPlace = (int) (Math.random() * 8);
+
+                if (checkPosition(queenPlace, array, i)) {
+                    array[i] = queenPlace;
+                    isTrue = false;
+                }
+                else{
+                    counter++;
+                    if(counter > 100){
+                       return true;
+                    }
+                }
+            }
+
+        }
+        return false;
+    }
+    public static void displayChessBoard(int[] array){
+        for(int i = 0; i < array.length; i++){
+            for(int j = 0; j < array.length; j++){
+                if(j < array[i]){
+                    System.out.print("| ");
+                } else if (j == array[i]) {
+                    System.out.print("|Q");
+                } else if (j > array[i]) {
+                    System.out.print("| ");
+                }
+            }
+            System.out.print("|");
+            System.out.println();
+        }
+    }
 }
