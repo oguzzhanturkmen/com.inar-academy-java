f00d81fc93b69f5d60637959afa96a14a99c2ae0
Ali Oğuzhan Türkmen <111460897+oguzzhanturkmen@users.noreply.github.com>
2022-11-15 16:17:06 +0300

    Added exercises

---
+++ src/chapters/chapter_11/Employee.java
@@ -0,0 +1,52 @@
+package chapters.chapter_11;
+
+import chapters.chapter_10.MyDate;
+
+public class Employee extends Person{
+
+    private double officeSalary;
+    private MyDate dateHired;
+
+    public Employee() {
+        super();
+        dateHired = new MyDate();
+        officeSalary = 0;
+    }
+
+    public Employee(String name, String adress, String phoneNumber, String emailAdress) {
+        super(name, adress, phoneNumber, emailAdress);
+        dateHired = new MyDate();
+        officeSalary = 0;
+    }
+
+    public Employee(String name, String adress, String phoneNumber, String emailAdress, double officeSalary) {
+        super(name, adress, phoneNumber, emailAdress);
+        this.officeSalary = officeSalary;
+        this.dateHired = new MyDate();
+    }
+
+    public double getOfficeSalary() {
+        return officeSalary;
+    }
+
+    public void setOfficeSalary(double officeSalary) {
+        this.officeSalary = officeSalary;
+    }
+
+    public String getDateHired() {
+        return dateHired.getDay() + "," + dateHired.getMonth() + "," +dateHired.getYear();
+    }
+
+    public void setDateHired() {
+        this.dateHired = new MyDate();
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() +" " +
+                "Employee{" +
+                "officeSalary=" + officeSalary +
+                "Date is : " + getDateHired() +
+                '}';
+    }
+}
---
+++ src/chapters/chapter_11/Exercise_02.java
@@ -0,0 +1,19 @@
+package chapters.chapter_11;
+
+public class Exercise_02 {
+    public static void main(String[] args) {
+
+        Person person = new Person("Oguzhan" , "İstanbul" , "05445444444" , "oguzhan@oguzhan.com");
+        Student student = new Student("Mete" , "Bursa" , "05333333333" , "mete@mete.com" ,Student.JUNIOR );
+        Employee employee = new Employee("Ahmet" , "Ankara" ,"05222222222" ,"ahmet@ahmet.com", 2500 );
+        Faculty faculty = new Faculty("İhsan" , "İstanbul","05465464646","ihsan@ihsan.com",4500,"12","Senior");
+        Staff staff = new Staff("Sefa" , "Ankara" , "05444444444" , "sefa@sefa.com",5200,"Professor");
+
+        System.out.println(person.toString());
+        System.out.println(student.toString());
+        System.out.println(employee.toString());
+        System.out.println(faculty.toString());
+        System.out.println(staff.toString());
+
+    }
+}
