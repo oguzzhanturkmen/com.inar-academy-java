7f7070137a4a46804618b7af165ebb4b2f1c6977
Ali Oğuzhan Türkmen <111460897+oguzzhanturkmen@users.noreply.github.com>
2022-11-18 20:57:08 +0300

    Added exericses

---
+++ src/weeks/exercise_002/Staff.java
@@ -0,0 +1,26 @@
+package weeks.exercise_002;
+
+public class Staff extends Employee {
+
+    private String title;
+
+    public Staff(String name, String adress, String phoneNumber, String emailAdress, double officeSalary, String title) {
+        super(name, adress, phoneNumber, emailAdress, officeSalary);
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + " " + "Staff{" +
+                "title='" + title + '\'' +
+                '}';
+    }
+}
---
+++ src/weeks/exercise_02/Exercise_02.java
@@ -0,0 +1,2 @@
+package weeks.exercise_02;public class Exercise_02 {
+}
